c740e10cf5cafb9898fcc6966e0935feb57b3ed3
Thomas Kang <tkang9285@gmail.com>
2019-12-04 16:11:28 -0500

    updated

---
+++ models/project.js
@@ -0,0 +1,25 @@
+module.exports = (sequelize, DataTypes) => {
+  const Project = sequelize.define('Project', {
+    title: DataTypes.STRING,
+    imageUrl: DataTypes.STRING,
+    description: DataTypes.STRING,
+    githubUrl: DataTypes.STRING,
+    deployedUrl: DataTypes.STRING,
+    userId: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'User',
+        key: 'id',
+        as: 'userId',
+      }
+    }
+  }, {});
+  Project.associate = function (models) {
+    // associations can be defined here
+    Project.belongsTo(models.User, {
+      foreignKey: 'userId',
+      onDelete: 'CASCADE'
+    })
+  };
+  return Project;
+};
